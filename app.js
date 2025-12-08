b8896f141f09eae0477771da27d99667a303b264
Donapoparg <agusparaversifunciona@gmail.com>
2025-12-07 21:07:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,5 @@
 import React, { useState, useEffect, useRef, memo } from 'https://esm.sh/react@18.2.0';
 import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client';
 // ... todo el código hasta el final ...
-import { jsx as _jsx } from "react/jsx-runtime";
 root.render(/*#__PURE__*/_jsx(App, {}));
 // FIN DEL CÓDIGO A COPIAR
